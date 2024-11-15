0a8f4bdb95440e31bd2a92b4e897b60d5d27f697
f5aaff <f5aaff@git>
2024-11-15 18:53:04 +0000

    updated ignore, takes a basic convo over cli using a buffer reader.

---
+++ README.md
@@ -0,0 +1,10 @@
+#GO-LLM
+
+basic project, utilizes the go-llama.cpp bindings.
+
+
+#TODO:
+-TODO: now that it can query a model, a means of persistent conversation with the model.
+-TODO: front end needed
+-TODO: API potentially, to handle queries in a server/client format
+-TODO: web scraping - this thing needs the capacity to go poke the web for stuff
--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"strings"
 
 	llama "github.com/go-skynet/go-llama.cpp"
 )
@@ -34,9 +36,47 @@ func load_config(cfg *Config) error {
 	return nil
 }
 
+func takeInput(history *[]string) error {
+	fmt.Print("msg: ")
+	reader := bufio.NewReader(os.Stdin)
+	userIn,err := reader.ReadString('\n')
+	if err != nil {
+		return err
+	}
+	*history = append(*history, "User: "+userIn)
+	return nil
+}
+
+func genResponse(history *[]string, model *llama.LLama) error {
+	prompt := strings.Join(*history, "\n") + "\n:Assistant:"
+	resp, err := model.Predict(prompt, llama.SetTemperature(0.7), llama.SetTopK(50), llama.SetTokens(200))
+	if err != nil {
+		return err
+	}
+
+	*history = append(*history, "Assistant: "+resp)
+	fmt.Println("Assistant: "+resp)
+	return nil
+}
+
+func initConversation(history *[]string, model *llama.LLama) error {
+	println("starting conversation...")
+	for {
+		err := takeInput(history)
+		if err != nil {
+			println("TAKEINPUT ERROR")
+			return err
+		}
+		err = genResponse(history, model)
+		if err != nil {
+			println("GENRESPONSE ERROR")
+			return err
+		}
+	}
+}
 func main() {
 	cfg := Config{
-		ModelPath: "./models/TinyLLama-1.1B-Chat-v1.0-GGUF/ggml-model-f16.gguf",
+		ModelPath:  "./models/TinyLLama-1.1B-Chat-v1.0-GGUF/ggml-model-f16.gguf",
 		InitPrompt: "You are a chat bot, intended to answer general queries.",
 	}
 
@@ -50,12 +90,9 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to load model: %v", err)
 	}
-
-	// Prompt for the model
-	prompt := cfg.InitPrompt
-	result, err := model.Predict(prompt, llama.SetThreads(4))
+	history := make([]string, 1)
+	err = initConversation(&history, model)
 	if err != nil {
-		log.Fatalf("Prediction error: %v", err)
+		log.Printf("crash out, shit's fucked: %s\n", err.Error())
 	}
-	fmt.Printf("Model Response: %s\n", result)
 }
